27de50eedd0c9a10c85e5c31085f9c5cfcfa3d89
Akshay Kumar <akshaykumarragavan@gmail.com>
2021-10-12 08:04:39 +0400

    after discord.js tutorial
    finished the discord js tutorial, need to clear from here on out

--- dissonance-discord/README.md
+++ dissonance-discord/README.md
@@ -82,3 +82,19 @@ also installed `discordjs/rest` and `discord-api-types`
 > A token is essentially your bot's password; it's what your bot uses to login to Discord. With that said, it is vital that you do not ever share this token with >anybody, purposely or accidentally. If someone does manage to get a hold of your token, they can use your bot as if it were theirs—this means they can perform malicious >acts with it.
 
 **I think .gitignore is just relative path anyways**
+
+
+![watch pong](ping_slash.png)
+
+>Discord allows developers to register slash commands, which provide users a first-class way of interacting directly with your application. Before being able to reply to a command, you must first register it.
+
+
+![start / dev scripts](scripts.png)
+```
+npm run start
+```
+
+also installed nodemon
+`npm install nodemon`
+
+`const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES"]});`
--- dissonance-discord/bih-sniper/src/bot.js
+++ dissonance-discord/bih-sniper/src/bot.js
@@ -1,51 +1,85 @@
 /* 
 
 Create a Discord Bot with Node.js
-
+ORIGINAL BOT
 */
 
 // dotenv package
 require("dotenv").config();
 
-console.log(process.env.DISCORDJS_BOT_TOKEN);
+const {Client} = require('discord.js');
+// had to add intents
+const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const 	PREFIX = "º";
+
+
+// Registering Event (READY)
+client.on('ready', () => {
+	console.log(`${client.user.username} // ${client.user.tag} has logged in!`)
+})
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
+// we need to ignore bot self message.
+// sent : hello
+// reply : hello
+client.on('messageCreate', (message) => {
+	if (message.author.bot === true) {return}
+	console.log(`${message.author.tag} sent a message"${message}"`)
+	if(message.content === `oppxi is gae`) {
+		message.reply('Indeed!');
+	}
+	if(message.content.startsWith(PREFIX)) {
+		// destructuring args [array] (spreader operator)
+		const [CMD_NAME, ...args] = message.content
+		.trim()
+		.substring(PREFIX.length)
+		.split(/\s+/);
+
+		if (CMD_NAME == 'kick') {
+			if (message.member.hasPermission('KICK MEMBERS')) 
+				return message.reply('You do not have permissions to use the command');
+			if (args.length === 0) return message.reply('Please Provide an ID');
+			const member = message.guild.members.cache.get(args[0])
+			if (member) {
+				member
+					.kick();
+					.then((member) => message.channel.send(`${member} was kicked.`));
+					.catch((err) => message.channel.send(`I  do not have the permissions to kick user :(`));
+			} else {
+				message.reply('That member was not found!')
+			}
+			message.channel.send(`kicked the user`)
+		}
+		if (CMD_NAME == 'ban') {
+			message.channel.send(`banned the user`)
+		}
+		console.log(CMD_NAME);
+		console.log(args);
+
+	}
+	
+		
+
+	
+});
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
 
 // Register a slash command against the Discord API?
 // slash commands are messages that trigger a HTTP request to a web service.
 
-// function
-const { REST } = require("@discordjs/rest");
-// object
-const { Routes } = require("discord-api-types/v9");
-
-// console.log(typeof REST, typeof Routes)
-
-// some sort of dynamic dictionary. commands[0] instances
-const commands = [
-	{
-		name: "ping",
-		description: "Replies with pong!",
-	},
-];
-
-const rest = new REST({ version: "9" }).setToken("token");
-
-console.log(rest.cdn);
-
-let simple_var = "okay okay okay okay";
-
-// okay okay okay okay first format on save
-// format document on Ctrl + Shift + I
-
-(async () => {
-	try {
-		console.log("Started refreshing application (/) commands.");
-
-		await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
-			body: commands,
-		});
-
-		console.log("Successfully reloaded application (/) commands.");
-	} catch (error) {
-		console.error(error);
-	}
-})();
